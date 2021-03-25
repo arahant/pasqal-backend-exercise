2e5fb59e484bcf376e6f8f7ceba1937bfd304d4d
Arahant <arahant.ashok@gmail.com>
2021-03-25 08:59:12 -0400

    [queue]
    1. queue.py - push, pop
    2. error.py - ErrorEmptyQueue, ErrorInvalidData

--- exercise/queue.py
+++ exercise/queue.py
@@ -1,3 +1,4 @@
+from exception import ErrorEmptyQueue, ErrorInvalidData
 import heapq
 
 class PriorityQueue():
@@ -7,9 +8,30 @@ class PriorityQueue():
         self.time = 0
 
     def push(self, item, priority):
+        """
+        Users can push items with a priority to the queue
+        """
+        # if item is NULL, throw exception
+        if not item:
+            raise ErrorInvalidData('Invalid item')
+
+        # if priority is NULL or NEGATIVE, throw exception
+        if not priority or priotity < 0:
+            raise ErrorInvalidData('Invalid priority')
+
         self.time += 1
-        self.
-        raise NotImplementedError
+        val = (-priority, self.time, item)
+        heapq.heappush(self.queue, val)
+
 
     def pop(self):
-        raise NotImplementedError
+        """
+        When popping an item from the queue, we get the item with the highest priority.
+        If there are many with the same priority, we return the oldest.
+        """
+        # if queue is empty, throw exception
+        if not self.queue:
+            raise ErrorEmptyQueue("Empty queue")
+
+        priority, time, item = heapq.heappop(self.queue)
+        return item
--- exercise/worker.py
+++ exercise/worker.py
@@ -1,6 +1,7 @@
 class Worker():
 
     def run():
-        """wait for new jobs on the queue and send them to the right device
+        """
+        wait for new jobs on the queue and send them to the right device
         """
         raise NotImplementedError
