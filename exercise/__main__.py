856663c8a2b4ea9927a835d541ca4dd6d29c0067
Arahant <arahant.ashok@gmail.com>
2021-03-25 17:39:27 -0400

    [pool-manager] __init__.py > __main__.py

--- exercise/__main__.py
+++ exercise/__main__.py
@@ -1,25 +1,32 @@
+
+import os
+import sys
+import logging
+import threading
+from flask import Flask
+
 from exercise.queue import PriorityQueue
 from exercise.api import API
-import threading
-
 from exercise import utils
 from exercise.pool import DevicePool
 from exercise.job import Job
 from exercise.worker import Worker
 
-# The actual program
+# Create Flask application
+app = Flask(__name__)
+
+app.config.from_object("config")
+app.config['LOGGING_LEVEL'] = logging.INFO
+app.logger.info("Scheduler inititalized!")
+
+queue = PriorityQueue()
+pool = DevicePool()
+
 if __name__ == '__main__':
-    print('Starting the scheduler')
-
-    pool = DevicePool()
-    queue = PriorityQueue()
-
-    # Running the api should not block. You should run it asynchronously
-    # using threading, asyncio, or any other library you see fit.
-    # e.g. with threading
-    api = API()
-    threading.Thread(target=api.run)
+    # Running the API calls asynchronously
+    # api = API()
+    # threading.Thread(target=api.run)
 
     # TODO:
-    worker = Worker()
-    worker.run()
+    # worker = Worker()
+    # worker.run()
