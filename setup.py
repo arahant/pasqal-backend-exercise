7948a7288bdb414aae4cb74f1122e3be148cea64
Arahant <arahant.ashok@gmail.com>
2021-03-26 09:34:51 -0400

    [job] nosetests

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=["flask", "Flask", "Flask-API", "flask-restplus", "pytest", "psycopg2-binary", "Werkzeug", "pylint"],
+    install_requires=["flask", "Flask", "Flask-API", "flask-restplus", "pytest", "psycopg2-binary", "Werkzeug", "pylint", "nose", "pinocchio", "factory-boy"],
     python_requires=">=3.8.0",
 )
